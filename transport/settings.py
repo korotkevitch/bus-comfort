38596f6fb0117b955e704854c19b6a7bc7120707
Igor Korotkevitch <i.korotkevitc@gmail.com>
2021-06-15 09:49:06 +0300

    Added '*' to ALLOWED_HOSTS

--- transport/settings.py
+++ transport/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'bus-comfort.by']
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'bus-comfort.by']
 
 
 # Application definition
